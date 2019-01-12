46299adae4dc571c5776c8bafd79bd5ca489998e
NabilMms <nabil.barouche@epitech.eu>
2019-01-12 10:57:45 +0100

    create obj data neo and component charts

---
+++ src/components/barCharts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class barCharts extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default barCharts;
