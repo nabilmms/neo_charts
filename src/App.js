7591e7c81018dec6c1b60d57f891a224dd713af1
NabilMms <nabil.barouche@epitech.eu>
2019-01-12 11:04:59 +0100

    send neo data to charts components AND install google react charts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import neoData from './data/neo_data';
-import barCharts from './components/barCharts';
+import BarCharts from './components/barCharts';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello Botify !</h1>
+          <BarCharts neoData={neoData}/>
       </div>
     );
   }
--- src/components/barCharts.js
+++ src/components/barCharts.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Chart from 'react-google-charts';
 
 class barCharts extends Component {
+    constructor(props){
+        super(props);
+        console.log(props.neoData);
+    }
+
     render() {
         return (
             <div>
-
+                <Chart/>
             </div>
         );
     }
